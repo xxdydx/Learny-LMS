c055d5826805a2a5563c60521e955afd092f695b
xxdydx <official@arul.me>
2023-04-04 13:24:57 +0800

    feat: rendering of course page based on sample data

--- frontend/src/app/components/Contents.tsx
+++ frontend/src/app/components/Contents.tsx
@@ -1,28 +1,7 @@
 import { Link, List, ListItem } from "@mui/material";
 import { useState } from "react";
+import { sampleCourse } from "../data/courseinfo";
 
-const chapters = [
-  {
-    id: 0,
-    name: "Price Mechanism",
-    quantity: 2,
-  },
-  {
-    id: 1,
-    name: "Market Failure",
-    quantity: 1,
-  },
-  {
-    id: 2,
-    name: "Firms & Decisions",
-    quantity: 1,
-  },
-  {
-    id: 3,
-    name: "Globalisation",
-    quantity: 2,
-  },
-];
 /**
  * MenuInteractiveColumn :
  * @description
@@ -35,20 +14,20 @@ const Contents = () => {
   return (
     <div className="w-fit hidden md:flex md: ml-4 lg:ml-6">
       <List>
-        {chapters.map((value) => {
+        {sampleCourse.chapters.map((value) => {
           return (
             <ListItem key={value.id}>
               <Link
-                href={`#${value.name}`}
+                href={`#${value.title}`}
                 underline="none"
                 className={
-                  selected === value.name
+                  selected === value.title
                     ? "text-white font-bold"
                     : "dark:text-slate-400 text-slate-700"
                 }
-                onClick={() => handleClick(value.name)}
+                onClick={() => handleClick(value.title)}
               >
-                {value.name}
+                {value.title}
               </Link>
             </ListItem>
           );
