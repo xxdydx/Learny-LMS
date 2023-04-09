a42afd627263d1b7c565196d4ce8c76ba4d7ce37
xxdydx <official@arul.me>
2023-04-09 12:00:56 +0800

    refactor: added routers and middleware to main app file

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import { connectToDatabase } from "./utils/db";
+import usersRouter from "./controllers/users";
+import coursesRouter from "./controllers/courses";
+import loginRouter from "./controllers/login";
+import { PORT } from "./utils/config";
+const { errorHandler } = require("./utils/middleware");
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3001;
-
-app.get("/ping", (req, res) => {
-  console.log("ping");
-  res.send("pong");
-});
+app.use("/api/courses", coursesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
   await connectToDatabase();
