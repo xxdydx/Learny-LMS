75243af5b9b1bd57bce94b9219d27ce0df19438c
xxdydx <official@arul.me>
2023-04-09 11:59:41 +0800

    various fixes

--- backend/utils/config.ts
+++ backend/utils/config.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const DATABASE_URL: string = process.env.DATABASE_URL as string;
 export const PORT = process.env.PORT || 3001;
+export const SECRET = `56.R>_~.RX'U/X;yGbxZmYDpe@w7J9YRz2qy$/4b4;\v5-?a&Nj7zeH55AjA`;
