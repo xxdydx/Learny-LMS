61cd8c100d5c17c586ecf14ad57399a2bb3777a4
xxdydx <official@arul.me>
2023-04-12 22:38:56 +0800

    feat: defined model relationships
    - teacher/creator & course
    - course & students

--- backend/models/index.ts
+++ backend/models/index.ts
@@ -3,10 +3,17 @@ import Chapter from "./chapter";
 import Section from "./section";
 import File from "./file";
 import User from "./user";
+import CourseStudent from "./coursestudent";
 
+// for creator of course - e.g. Teacher
 User.hasMany(Course);
-Course.belongsTo(User);
+Course.belongsTo(User, { as: "teacher" });
 
+// for mapping students to the course
+Course.belongsToMany(User, { through: CourseStudent });
+User.belongsToMany(Course, { through: CourseStudent });
+
+// for the various sub-sections under each course
 Course.hasMany(Chapter);
 Chapter.belongsTo(Course);
 
