7aa0f4770170ae9d89a97b1b7fcd9bac76b27e5e
xxdydx <official@arul.me>
2023-07-02 19:03:03 +0800

    feat: removal of students from course

--- backend/controllers/enrollment.ts
+++ backend/controllers/enrollment.ts
@@ -68,4 +68,62 @@ router.post("/", tokenExtractor, async (req: CustomRequest, res, next) => {
   }
 });
 
+// To remove a student into the course
+router.delete("/", tokenExtractor, async (req: CustomRequest, res, next) => {
+  try {
+    // Check if logged in user (aka the teacher) has permissions to add students
+    const teacher = req.user;
+    const course = await Course.findByPk(req.body.courseId);
+    if (!teacher) {
+      return res.status(401).send("User not logged in");
+    }
+    if (!course) {
+      return res.status(404).send("Course not found");
+    }
+    if (teacher.id.toString() !== course.teacherId.toString()) {
+      return res
+        .status(403)
+        .send(
+          `You don't have permissions to delete students from this course.`
+        );
+    }
+
+    // Check if student exists
+
+    const student_id = req.body.studentId;
+
+    const student = await User.findOne({
+      where: { id: student_id },
+    });
+    if (!student) {
+      return res.status(404).send("Student not found");
+    }
+    // Check if student is actually a student
+    if (student.role !== "student") {
+      return res
+        .status(403)
+        .send(`Only allowed to delete students to this course`);
+    }
+
+    const userId = student.id;
+    const courseId = req.body.courseId;
+
+    const deleteEnrollmentQuery = `
+    DELETE FROM "enrollments"
+    WHERE "user_id" = $1 AND "course_id" = $2
+  `;
+    await Enrollment.sequelize?.query(deleteEnrollmentQuery, {
+      bind: [userId, courseId],
+      type: QueryTypes.DELETE,
+    });
+    const editedCourse = await getUpdatedCourse(course.id);
+    if (!editedCourse) {
+      return res.status(404).send("Course not found");
+    }
+    return res.json(editedCourse);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
