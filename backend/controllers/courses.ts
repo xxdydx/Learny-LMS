bd7733b466ddb0ea652eb5f214634382293049ca
Arul <official@arul.me>
2024-05-20 17:49:07 +0800

    refactor: fetching of all courses

--- backend/controllers/courses.ts
+++ backend/controllers/courses.ts
@@ -13,6 +13,7 @@ import { tokenExtractor } from "../utils/middleware";
 import { CustomRequest } from "../types";
 import { Op } from "sequelize";
 import getUpdatedCourse from "../utils/getUpdatedCourse";
+import { getCoursesByRole } from "../utils/courseFetching";
 
 const router = express.Router();
 
@@ -20,287 +21,11 @@ const router = express.Router();
 router.get("/", tokenExtractor, async (req: CustomRequest, res, next) => {
   try {
     const user = req.user;
-
     if (!user) {
       return res.status(404).send("User not found");
     }
-    // GET all courses for admin users
-    if (user.role === "admin") {
-      const courses = await Course.findAll({
-        attributes: { exclude: ["teacherId"] },
-        include: [
-          {
-            model: User,
-            as: "teacher",
-            attributes: ["name", "username", "id"],
-          },
-          {
-            model: Chapter,
-            as: "chapters",
-            attributes: ["title", "id"],
-            include: [
-              {
-                model: Section,
-                as: "sections",
-                include: [
-                  {
-                    model: File,
-                    as: "files",
-                    attributes: ["name"],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        order: [
-          ["createdAt", "DESC"],
-          [{ model: Chapter, as: "chapters" }, "createdAt", "DESC"],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: File, as: "files" },
-            "createdAt",
-            "ASC",
-          ],
-        ],
-      });
-      return res.send(courses);
-    }
-
-    // GET courses created by a teacher
-    if (user.role === "teacher") {
-      const courses = await Course.findAll({
-        attributes: { exclude: ["teacherId"] },
-        where: {
-          teacherId: {
-            [Op.eq]: user.id,
-          },
-          template: { [Op.not]: true },
-        },
-
-        include: [
-          {
-            model: User,
-            as: "teacher",
-            attributes: ["name", "username", "id", "email", "role"],
-          },
-          {
-            model: User,
-            as: "students",
-            attributes: ["name", "username", "id", "email", "role"],
-            through: {
-              attributes: [],
-            },
-          },
-          {
-            model: Announcement,
-            as: "announcements",
-            required: false,
-            where: {
-              expiry: {
-                [Op.gt]: new Date(),
-              },
-            },
-          },
-          {
-            model: Chapter,
-            as: "chapters",
-            attributes: ["title", "id", "createdAt", "pinned"],
-            include: [
-              {
-                model: Section,
-                as: "sections",
-                include: [
-                  {
-                    model: File,
-                    as: "files",
-                  },
-                  {
-                    model: Assignment,
-                    as: "assignments",
-                    include: [
-                      {
-                        model: Submission,
-                        as: "submissions",
-                        required: false,
-                        include: [
-                          {
-                            model: User,
-                            as: "student",
-                            attributes: [
-                              "name",
-                              "username",
-                              "id",
-                              "email",
-                              "role",
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        order: [
-          ["createdAt", "DESC"],
-          [{ model: Chapter, as: "chapters" }, "createdAt", "DESC"],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: File, as: "files" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: Assignment, as: "assignments" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: Assignment, as: "assignments" },
-            { model: Submission, as: "submissions" },
-            "createdAt",
-            "ASC",
-          ],
-        ],
-      });
-
-      return res.send(courses);
-    }
-
-    // GET students' courses
-    if (user.role === "student") {
-      const courses = await Course.findAll({
-        attributes: { exclude: ["teacherId"] },
-        where: {
-          template: { [Op.not]: true },
-        },
-        include: [
-          {
-            model: User,
-            as: "teacher",
-            attributes: ["name", "username", "id", "email", "role"],
-          },
-          {
-            model: User,
-            as: "students",
-            where: {
-              id: {
-                [Op.eq]: user.id,
-              },
-            },
-            attributes: ["name", "username", "id", "email", "role"],
-            through: {
-              attributes: [],
-            },
-          },
-          {
-            model: Chapter,
-            as: "chapters",
-            attributes: ["title", "id", "pinned"],
-            include: [
-              {
-                model: Section,
-                as: "sections",
-
-                include: [
-                  {
-                    model: File,
-                    as: "files",
-                    required: false,
-                    where: {
-                      visibledate: {
-                        [Op.lt]: new Date().toISOString(),
-                      },
-                    },
-                  },
-                  {
-                    model: Assignment,
-                    as: "assignments",
-                    required: false,
-                    where: {
-                      visibledate: {
-                        [Op.lt]: new Date().toISOString(),
-                      },
-                    },
-                    include: [
-                      {
-                        model: Submission,
-                        as: "submissions",
-                        required: false,
-                        where: {
-                          studentId: {
-                            [Op.eq]: user.id,
-                          },
-                        },
-                        include: [
-                          {
-                            model: User,
-                            as: "student",
-                            attributes: [
-                              "name",
-                              "username",
-                              "id",
-                              "email",
-                              "role",
-                            ],
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        order: [
-          ["createdAt", "DESC"],
-          [{ model: Chapter, as: "chapters" }, "createdAt", "DESC"],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: File, as: "files" },
-            "createdAt",
-            "ASC",
-          ],
-          [
-            { model: Chapter, as: "chapters" },
-            { model: Section, as: "sections" },
-            { model: Assignment, as: "assignments" },
-            "createdAt",
-            "ASC",
-          ],
-        ],
-      });
-      return res.send(courses);
-    }
+    const courses = await getCoursesByRole(user);
+    return res.send(courses);
   } catch (err) {
     return next(err);
   }
@@ -422,7 +147,6 @@ router.post(
   }
 );
 
-
 router.post(
   "/:id/announcements",
   tokenExtractor,
@@ -439,7 +163,9 @@ router.post(
       if (user.id.toString() !== course.teacherId.toString()) {
         return res
           .status(403)
-          .send(`You don't have permissions to create announcements for this course.`);
+          .send(
+            `You don't have permissions to create announcements for this course.`
+          );
       }
 
       await Announcement.create({
